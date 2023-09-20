774a131abb62305abf035e042ffa53f000132097
Savanna Whittaker <129790884+savannamw2@users.noreply.github.com>
2023-09-19 18:07:03 -0600

    Add files via upload

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,37 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Savanna Whittaker';
+let currentYear = new Date().getFullYear();
+const profilePicture = 'images/profilePictu.jpg';
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const pictureElement = document.querySelector('picture');
+const imageElement = pictureElement.querySelector('img');
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`; 
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+
+const altText = `Profile image of Savanna Whittaker`;
+imageElement.setAttribute('alt', altText);
+
+
+/* Step 5 - Array */
+
+let favFoods = ['Sushi', ' Breakfast Sandwiches', ' Pumpkin Cookies'];
+foodElement.innerHTML = `${favFoods}`;
+favFoods.push(' Grilled Chicken');
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
+
+
